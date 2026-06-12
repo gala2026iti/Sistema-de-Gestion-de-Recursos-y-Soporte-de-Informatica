3b48f745a93ccddb10679e616d352b3f6ab9e435
“Manu” <abraham2009mm@gmail.com>
2026-06-11 23:02:53 -0300

    fixes: Resolve #6. Se implementan mejoras con respecto al funcionamiento de los roles, eliminando la función de eliminar botones de un mismo inicio, siendo esto reemplazado por una interfaz de inicio distinta para cada rol correspondiente. Se realizaron modificaciones en los js para modificar las redirecciones y eliminar residuos de viejas funcionalidades. Se movieron los archivos home de sus carpetas de rol designado, por lo que tocó modificarlos para que la ruta hacia sus distintos recursos relacionados funcionara correctamente.

--- SGRSI/assets/js/inicioSesion.js
+++ SGRSI/assets/js/inicioSesion.js
@@ -13,21 +13,27 @@ form.addEventListener("submit", function(e){
         rol: rol.value
     }
 
+    sessionStorage.setItem("usuario", JSON.stringify(usuario));
+
+
 if(usuario.cedula == 12345678 && usuario.clave === "docenteITI"){ // Rol docente
     usuario.rol = "docente"
+    window.location.href = "homeDocente.html"
+
 } else if (usuario.cedula == 23456789 && usuario.clave === "directorITI"){ // Rol director
     usuario.rol = "director"
+    window.location.href = "homeDirector.html"
+
 } else if (usuario.cedula == 34567890 && usuario.clave === "adminITI") { // Rol administrador
     usuario.rol = "admin"
+    window.location.href = "homeAdmin.html"
+
 } else if (usuario.cedula == 87654321 && usuario.clave === "asistenteITI"){ // Rol tecnico
     usuario.rol = "asistente"
+    window.location.href = "homeAsistente.html"
+
 } else {
     alert("Usuario o Contraseña incorrectos")
-}
 
-if(usuario.rol === "docente" || usuario.rol === "director" || usuario.rol === "admin" || usuario.rol === "asistente"){
-sessionStorage.setItem("rol", usuario.rol);
-window.location.href = "formulario.html"
 }
 })
-
