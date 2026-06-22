405e1ee203c6dc2399ce120cb53f230e27103dab
jeregpruebas-cpu <jeregpruebas@gmail.com>
2026-06-22 09:53:23 -0300

    fix: se adapta los cambios de navegacion de paz que con anterioridad se habrian perdido

--- SGRSI/assets/js/btn-menu-celular.js
+++ SGRSI/assets/js/btn-menu-celular.js
@@ -1,21 +1,42 @@
-// VARIABLES
-const btnMenu = document.getElementById("btn-menu")
-const btnCerrar = document.getElementById("btn-cerrar")
-const menu = document.querySelector(".nav-menu")
+const btnMenu = document.getElementById("btn-menu");
 
-// EVENTOS
-if (btnMenu && menu) {
-    btnMenu.addEventListener("click", function () {
-        menu.classList.add("activo")
-        btnMenu.style.display = "none"
-        if (btnCerrar) btnCerrar.style.display = "flex"
-    })
+const btnCerrar = document.getElementById("btn-cerrar");
+
+const menu = document.querySelector(".nav-menu");
+
+btnMenu.addEventListener("click", function () {
+
+    menu.classList.add("activo");
+
+    btnMenu.style.display = "none";
+
+    btnCerrar.style.display = "flex";
+});
+
+btnCerrar.addEventListener("click", function () {
+
+    menu.classList.remove("activo");
+
+    btnCerrar.style.display = "none";
+
+    btnMenu.style.display = "flex";
+});
+
+btnCerrar.style.display ="none";
+
+
+
+function comprobarPantalla() {
+    if (window.innerWidth > 1046) {
+        btnMenu.style.display = "none";
+        btnCerrar.style.display = "none";
+        menu.classList.remove("activo");
+    } else {
+        if (btnCerrar.style.display == "none"){
+            btnMenu.style.display = "flex";
+        }
+    }
 }
 
-if (btnCerrar && menu) {
-    btnCerrar.addEventListener("click", function () {
-        menu.classList.remove("activo")
-        btnCerrar.style.display = "none"
-        if (btnMenu) btnMenu.style.display = "flex"
-    })
-}
+comprobarPantalla();
+window.addEventListener("resize", comprobarPantalla);
