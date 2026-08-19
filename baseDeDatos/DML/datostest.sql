c45ed799d2ed0e32c477d9698215565f6d580074
Manu <abraham2009mm@gmail.com>
2026-08-19 20:57:16 -0300

    fix: Corrección de sentencias SQL

--- baseDeDatos/DML/datostest.sql
+++ baseDeDatos/DML/datostest.sql
@@ -1,19 +1,23 @@
-INSERT INTO USUARIO (ci, nombre, correo, clave, activo) VALUES
-('11111111', 'Admin Puro', 'admin@sgrsi.edu.uy', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 1),
-('22222222', 'Docente Puro', 'docente@sgrsi.edu.uy', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 1),
-('33333333', 'Tecnico Puro', 'tecnico@sgrsi.edu.uy', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 1),
-('44444444', 'Leandro', 'Leandro@sgrsi.edu.uy', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 1),
-('55555555', 'Usuario Inactivo', 'inactivo@sgrsi.edu.uy', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 0),
-('66666666', 'Usuario Sin Rol', 'sinrol@sgrsi.edu.uy', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 1);
+INSERT INTO USUARIO (ci, nombre, clave, activo) VALUES
+('11111111', 'Admin Puro', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 1),
+('22222222', 'Docente Puro', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 1),
+('33333333', 'Tecnico Puro', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 1),
+('44444444', 'Leandro', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS', 1);
 
 INSERT INTO ROL (ci, rol) VALUES
 ('11111111', 'administrador'),
 ('22222222', 'docente'),
 ('33333333', 'tecnico'),
 ('44444444', 'administrador'),
-('44444444', 'tecnico'),
-('55555555', 'docente')
+('44444444', 'tecnico')
 ;
+
+INSERT INTO CORREO (ci, correo) VALUES
+('11111111', 'admin@sgrsi.edu.uy'),
+('22222222', 'docente@sgrsi.edu.uy'),
+('33333333', 'tecnico@sgrsi.edu.uy'),
+('44444444', 'Leandro@sgrsi.edu.uy');
+
 
 
 INSERT INTO ADMINISTRADOR (ci) VALUES ('11111111'), ('44444444');
